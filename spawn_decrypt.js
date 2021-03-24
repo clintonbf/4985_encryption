46399587435be0cd86ac9a4a7c2949f41b7dc481
Clint <55072157+clintonbf@users.noreply.github.com>
2021-03-24 13:16:52 -0700

    Updated spawn_decrypt.js to support filename/command arg

--- spawn_decrypt.js
+++ spawn_decrypt.js
@@ -1,12 +1,13 @@
 const {spawn} = require('child_process');
 
-const encryptedMessage = "./encrypted_message";
+const SCRIPT = './python/dc_decrypt.py';
+const SOURCE = '-- filename';
+const ENCRYPTED_MESSAGE = "./encrypted_message";
 
-const python = spawn('python', ['./python/dc_decrypt.py', encryptedMessage]); // Will NOT work in Windows
+const python = spawn('python', [SCRIPT, SOURCE, ENCRYPTED_MESSAGE]); // Will NOT work in Windows
 
 python.stdout.on('data', (data) => {
     console.log(`Got data "${data.toString()}"`);
-    dataToSend = data.toString();
 });
 
 python.stderr.on('data', (data) => {
