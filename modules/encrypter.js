4c3e8d86c8a0a77b635445123a73d47444d4a678
Clint <55072157+clintonbf@users.noreply.github.com>
2021-03-29 15:51:01 -0700

    Added documentation

--- modules/encrypter.js
+++ modules/encrypter.js
@@ -1,6 +1,18 @@
+//N.B. this will NOT work on Windows
+
 const {spawn} = require('child_process');
 const fs = require('fs');
 
+const PYTHON_PATH = '../python';
+const ENCRYPT_SCRIPT = PYTHON_PATH + '/dc_encrypt.py';
+const DECRYPT_SCRIPT = PYTHON_PATH + '/dc_decrypt.py';
+
+/**
+ * Decrypts an encrypted file.
+ *
+ * @param {string} filename file to encrypt. Include path.
+ * @return {string} the file contents decrypted.
+ */
 exports.decryptFile = function(filename) {
     fs.readFile(filename, (err, data) => {
        if (err) {
@@ -11,37 +23,50 @@ exports.decryptFile = function(filename) {
     });
 }
 
+/**
+ * Decrypts a message.
+ *
+ * @param {string} message encrypted message
+ * @return {string} decrypted message. Returns null if there was an error or an exit (whatever that is)
+ */
 exports.decryptMessage = function(message) {
-    const SCRIPT = './python/dc_decrypt.py';
-
-    const python = spawn('python', [SCRIPT, message]); // Will NOT work in Windows
+    const python = spawn('python', [DECRYPT_SCRIPT, message]);
 
     python.stdout.on('data', (data) => {
         return (data.toString());
     });
 
     python.stderr.on('data', (data) => {
-        console.log(`Error occurred: ${data}`);
+        console.error(`Error occurred: ${data}`);
+        return null;
     })
 
     python.on('exit', (code) => {
         console.log(`Exit code ${code}`);
+        return null;
     });
 }
 
-exports.encryptObject = function(message) {
-    const SCRIPT = './python/dc_encrypt.py';
-    const python = spawn('python', [SCRIPT , message]); // Will NOT work in Windows
+/**
+ * Encrypts a message.
+ *
+ * @param {string} message to encrypt
+ * @return {string} encrypted message. Returns null if there was an error or an exit (whatever that is)
+ */
+exports.encryptMessage = function(message) {
+    const python = spawn('python', [ENCRYPT_SCRIPT , message]);
 
     python.stdout.on('data', (data) => {
         return data.toString;
     });
 
     python.stderr.on('data', (data) => {
-        console.log(`Error occurred: ${data}`);
+        console.error(`Error occurred: ${data}`);
+        return null;
     })
 
     python.on('exit', (code) => {
         console.log(`Exit code ${code}`);
+        return null;
     });
 }
